072a8804f858c662d9e7914e7ecf0c1a2ebd6eb3
Bagus Kurniawan <baguskurniawan999@gmail.com>
2020-04-29 23:27:43 +0700

    DONE

---
+++ application/views/pages/home.php
@@ -0,0 +1,39 @@
+<div class="row">
+    <div class="col-md-6">
+        <h1>Deskripsi</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Magni aut natus quo perferendis sequi ipsa ad culpa mollitia atque, 
+            sit eaque cum vel eos! Commodi et modi optio id eos?
+        </p>
+    </div>
+    <div class="col-md-6">
+        <h1>Desa</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Magni aut natus quo perferendis sequi ipsa ad culpa mollitia atque, 
+            sit eaque cum vel eos! Commodi et modi optio id eos?
+        </p>
+    </div>
+</div>
+<div class="row pb-5">
+    <div class="col-md-12">
+        <h1>Map Desa</h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.3608588781312!2d112.72498271419373!3d-7.31329687393589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fb774d9f665d%3A0x7034fd3dfb802e41!2sUnesa%20University%20Press!5e0!3m2!1sid!2sid!4v1588177394676!5m2!1sid!2sid" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+    <div class="col-md-12">
+        <h1>Dana Desa</h1>
+        <table class="table table-striped">
+            <tr>
+                <td>Tgl</td>
+                <td>Keperluan Desa</td>
+                <td>Dana</td>
+            </tr>
+            <tr>
+                <td>13/4/2020</td>
+                <td>Pembuatan Jalan</td>
+                <td>Rp 24.000.000</td>
+            </tr>
+        </table>
+    </div>
+</div>
